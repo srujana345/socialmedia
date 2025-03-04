5d6b86e4b2b7455f02bd9b2a072e3ab7aa91e4ed
srujana345 <sai.srujana144@gmail.com>
2025-03-04 14:31:40 +0530

    updated

--- script.js
+++ script.js
@@ -6,16 +6,15 @@ function showLogin() {
     .catch((err) => console.log(err));
 }
 
-
 function showPosts(id) {
-  let str = "";
+  let str = "<h3>My Post</h3>";
   //console.log(`https://jsonplaceholder.typicode.com/posts/userId=${id}`)
-  fetch(`https://jsonplaceholder.typicode.com/posts/?userId=${id}`)
+  fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     .then((res) => res.json())
     .then((data) => {
       data &&
         data.map((value) => {
-          str += `<div>
+          str += `
         <b>${value.title}</b>
         <p>${value.body}</p>
         </div>`;
@@ -29,7 +28,8 @@ function showProfile(id) {
   fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
     .then((res) => res.json())
     .then((data) => {
-      let str = `<div>
+      
+      let str = `<h3>My Profile</h3><div>
       <b>${data.name}</b>
       <p>${data.email}</p>
       </div>`;
@@ -41,7 +41,8 @@ function showAlbums(id) {
   fetch(`https://jsonplaceholder.typicode.com/albums?userId=${id}`)
     .then((res) => res.json())
     .then((data) => {
-      let str = "";
+      let str = "<h3>My Albums</h3>";
+      data &&
       data.map((value) => {
         str += `<div>
       <b>${value.title}</b>
@@ -51,28 +52,45 @@ function showAlbums(id) {
     })
     .catch((err) => console.log(err));
 }
+function toDo(id){
+  fetch(`https://jsonplaceholder.typicode.com/todos?userId=${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      let str = "<h3>My ToDos</h3>";
+      data &&
+      data.map((value) => {
+        str += `<div><input type='checkbox' ${value.completed && 'checked' }> 
+      <b>${value.title}</b>
+      </div>`;
+      });
+      content.innerHTML = str;
+    })
+    .catch((err) => console.log(err));
+
+}
+
+
 function showHome() {
-  let userId=selUser.value;
-  let str = `
+  let userId = selUser.value;
+  let userName = selUser.options[selUser.selectedIndex].text;
+  let str = `<div>
   <div class=' container-fluid'  >
     <div class='row'>
       <div class='d-flex justify-content-between bg-success text-white '>
         <div class='h1'> WebHouse</div>
-        <div class='h2' id='username'></div>
+        <div class='h2' id='username'>${userName}</div>
       </div>
     </div>
-    <div class='row'>
-      <div class='d-flex  h3'>
-      <div class='p-2 bg-light'>
+    <div class='d-flex flex-row'>
+      <div class='d-flex flex-column col-1 bg-light p-2'>
       <a href='#' class='text-black h3' onclick='showPosts(${userId})'>Home</a>
       <a href='#' class='text-black h3 ' onclick='showAlbums(${userId})'>Albums</a>
+      <a href='#' class='text-black h3 ' onclick='toDo(${userId})'>ToDos</a>
       <a href='#' class='text-black h3 ' onclick='showProfile(${userId})'>Profile</a>
       <a href='#' class='text-black h3 ' onclick='showLogin()'>Logout</a>
-      
       </div>
-      <div class='p-2' id='content'></div>
+      <div class='d-flex flex-column col-11' id='content'></div>
       </div>
-      
       </div>
       <div class='row'>
       <div class='d-flex justify-content-center bg-success text-white p-5 '>
@@ -83,6 +101,8 @@ function showHome() {
   </div>`;
   root.innerHTML = str;
   showPosts(userId);
+  toDo(userId);
+
 }
 
 function displayUsers(data) {
